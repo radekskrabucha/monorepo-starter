9f9deb6a01929b875f59e03a8e7c2a9059f9e1e6
radekskrabucha <rskrabucha13@gmail.com>
2025-03-24 13:11:54 -0500

    chore(eslint): update ESLint configuration to use shared config and add private flag in package.json

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,30 +1,6 @@
-import pluginJs from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import { baseConfig, reactConfig } from '@monorepo-starter/eslint-config'
 
 export default [
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-  {
-    languageOptions: { globals: globals.browser },
-    rules: {
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          "args": "all",
-          "argsIgnorePattern": "^_",
-          "caughtErrors": "all",
-          "caughtErrorsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "ignoreRestSiblings": true
-        }
-      ]
-    }
-  }
+  ...baseConfig,
+  ...reactConfig
 ]
