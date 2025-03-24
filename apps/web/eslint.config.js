65b5c0e468cc0f6ec9f74440a35158a891613ef0
radekskrabucha <rskrabucha13@gmail.com>
2025-03-24 13:07:24 -0500

    chore(eslint): update ESLint configuration to use shared config and add devDependency for eslint-config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,60 +1,8 @@
-import pluginJs from '@eslint/js'
-import pluginQuery from '@tanstack/eslint-plugin-query'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
-import reactPlugin from 'eslint-plugin-react'
-import reactCompiler from 'eslint-plugin-react-compiler'
-import reactHooksPlugin from 'eslint-plugin-react-hooks'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import { baseConfig, reactConfig } from '@monorepo-starter/eslint-config'
 
 const config = [
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginQuery.configs['flat/recommended'],
-  eslintConfigPrettier,
-  reactPlugin.configs.flat.recommended,
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react/jsx-key': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'jsx-a11y/alt-text': 'warn',
-      'jsx-a11y/aria-props': 'warn',
-      'jsx-a11y/aria-role': 'warn',
-      'jsx-a11y/role-has-required-aria-props': 'warn',
-      'react-compiler/react-compiler': 'error',
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          "args": "all",
-          "argsIgnorePattern": "^_",
-          "caughtErrors": "all",
-          "caughtErrorsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "ignoreRestSiblings": true
-        }
-      ]
-    },
-    plugins: {
-      'jsx-a11y': eslintPluginJsxA11y,
-      'react-hooks': reactHooksPlugin,
-      'react-compiler': reactCompiler
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
-  }
+  ...baseConfig,
+  ...reactConfig
 ]
 
 export default config
