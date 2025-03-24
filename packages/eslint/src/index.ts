46f6d8d072b4f3a96c0c93f586d6fcf14e88798d
radekskrabucha <rskrabucha13@gmail.com>
2025-03-24 13:04:05 -0500

    feat(eslint): enhance ESLint configuration with React and accessibility plugins

--- packages/eslint/src/index.ts
+++ packages/eslint/src/index.ts
@@ -1,5 +1,11 @@
 import pluginJs from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import reactPlugin from 'eslint-plugin-react'
+import reactCompiler from 'eslint-plugin-react-compiler'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import globals from 'globals'
 import { configs } from 'typescript-eslint'
 
 export const baseConfig: Array<unknown> = [
@@ -24,6 +30,37 @@ export const baseConfig: Array<unknown> = [
           ignoreRestSiblings: true
         }
       ]
+    }
+  }
+]
+
+export const reactConfig: Array<unknown> = [
+  ...pluginQuery.configs['flat/recommended'],
+  reactPlugin.configs.flat.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-key': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-role': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'react-compiler/react-compiler': 'error'
+    },
+    plugins: {
+      'jsx-a11y': eslintPluginJsxA11y,
+      'react-hooks': reactHooksPlugin,
+      'react-compiler': reactCompiler
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     }
   }
 ]
