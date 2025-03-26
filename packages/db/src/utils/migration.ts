747dd9413c5f1543cfbda5bf57acd8341f3047c8
radekskrabucha <rskrabucha13@gmail.com>
2025-03-26 16:55:47 -0500

    refactor(db): enhance runMigration function to dynamically resolve migrations folder path

--- packages/db/src/utils/migration.ts
+++ packages/db/src/utils/migration.ts
@@ -1,5 +1,13 @@
 import { migrate } from 'drizzle-orm/node-postgres/migrator'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { db } from '~db/index.js'
 
-export const runMigration = async (migrationsFolder: string) =>
-  await migrate(db, { migrationsFolder })
+export const runMigration = async () => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+
+  await migrate(db, {
+    migrationsFolder: path.resolve(__dirname, '../migrations')
+  })
+}
