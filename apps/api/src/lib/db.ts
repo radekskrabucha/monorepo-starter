fa1f9424c03c3404af537e6f72645b88f8bc2faa
radekskrabucha <rskrabucha13@gmail.com>
2025-03-26 13:07:27 -0500

    fix(db): use DRIZZLE_MIGRATIONS_FOLDER for migration path and add checks for folder existence

--- apps/api/src/lib/db.ts
+++ apps/api/src/lib/db.ts
@@ -1,19 +1,21 @@
 import { runMigration } from '@monorepo-starter/db/migration'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { env } from '~api/utils/env'
 
 export const runDbMigrations = async () => {
   try {
+    console.log('Migrating schema...')
+
+    if (!env.DRIZZLE_MIGRATIONS_FOLDER) {
+      console.log('No migrations folder provided, skipping migrations')
+      return
+    }
+
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = path.dirname(__filename)
 
-    console.log('Migrating schema...')
-    await runMigration(
-      path.resolve(
-        __dirname,
-        '../../node_modules/@monorepo-starter/db/src/migrations'
-      )
-    )
+    await runMigration(path.resolve(__dirname, env.DRIZZLE_MIGRATIONS_FOLDER))
     console.log('Schema migrated successfully')
   } catch (error) {
     console.error('Error migrating schema')
