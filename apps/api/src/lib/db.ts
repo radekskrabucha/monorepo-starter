4d20c36a069143848a3e7bb7a0d4af447ef2a989
radekskrabucha <rskrabucha13@gmail.com>
2025-03-26 16:56:06 -0500

    refactor(db): simplify runDbMigrations by removing folder path checks and directly invoking runMigration

--- apps/api/src/lib/db.ts
+++ apps/api/src/lib/db.ts
@@ -1,21 +1,9 @@
 import { runMigration } from '@monorepo-starter/db/migration'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { env } from '~api/utils/env'
 
 export const runDbMigrations = async () => {
   try {
     console.log('Migrating schema...')
-
-    if (!env.DRIZZLE_MIGRATIONS_FOLDER) {
-      console.log('No migrations folder provided, skipping migrations')
-      return
-    }
-
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
-
-    await runMigration(path.resolve(__dirname, env.DRIZZLE_MIGRATIONS_FOLDER))
+    await runMigration()
     console.log('Schema migrated successfully')
   } catch (error) {
     console.error('Error migrating schema')
