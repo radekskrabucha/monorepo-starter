9a64ea42e848e25e692e0d99bc4d3d8801d8815c
radekskrabucha <rskrabucha13@gmail.com>
2025-03-25 20:22:33 -0500

    refactor(db): update import statement for pg and restructure Pool initialization

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
+import pg from 'pg'
 import * as taskSchema from '~db/schema/task.schema.js'
 import { env } from '~db/utils/env.js'
+
+const { Pool } = pg
 
 const pool = new Pool({
   connectionString: env.DATABASE_URL
