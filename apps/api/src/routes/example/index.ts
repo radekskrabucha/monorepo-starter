996a14725ae1fee9eea5ca786a120f26f72e0a3d
radekskrabucha <rskrabucha13@gmail.com>
2025-05-07 18:20:26 +0200

    refactor(api/routes): update error response handling in example route to use createErrorResponse utility

--- apps/api/src/routes/example/index.ts
+++ apps/api/src/routes/example/index.ts
@@ -2,7 +2,7 @@ import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { z } from 'zod'
 import type { AppBindings } from '~api/types/app'
-import { errorResponse } from '~api/utils/error'
+import { ApiErrorType, createErrorResponse } from '~api/utils/error'
 
 const exampleSchema = z.object({
   name: z.string().min(1)
@@ -12,7 +12,10 @@ export const exampleRouter = new Hono<AppBindings>().basePath('/example').get(
   '/',
   zValidator('query', exampleSchema, (result, c) => {
     if (!result.success) {
-      return c.json(errorResponse({ message: 'Invalid query', ...result }), 400)
+      return c.json(
+        createErrorResponse('Invalid query', ApiErrorType.example.invalidQuery),
+        400
+      )
     }
   }),
   async c => {
