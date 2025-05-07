b5c349177618424eda72da8f60467acb283aea40
radekskrabucha <rskrabucha13@gmail.com>
2025-05-07 20:07:43 +0200

    refactor(api): remove database migration logic and related imports

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { runDbMigrations } from '~api/lib/db'
 import { notFound } from '~api/middleware/notFound'
 import { onError } from '~api/middleware/onError'
 import { pinoLogger } from '~api/middleware/pinoLogger'
@@ -9,8 +8,6 @@ import { serveEmojiFavicon } from '~api/middleware/serveEmojiFavicon'
 import { appRouter } from '~api/routes'
 import type { AppBindings } from '~api/types/app'
 import { env } from '~api/utils/env'
-
-runDbMigrations()
 
 export const app = new Hono<AppBindings>()
   .basePath('/api')
