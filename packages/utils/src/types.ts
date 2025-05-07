f31f4ad93f5b95944a3777e2c2dad140c9bf6b56
radekskrabucha <rskrabucha13@gmail.com>
2025-05-07 17:24:03 +0200

    feat(utils/types): add StringUnion type for extracting string union types from objects

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -3,3 +3,11 @@ export type NoEmpty<T> = T extends null | false | undefined ? never : T
 export type StringWithAutoCompleteOptions<T extends string> = (string & {}) | T
 
 export type ArrayElementType<T> = T extends readonly (infer U)[] ? U : never
+
+export type StringUnion<T> = {
+  [K in keyof T]: T[K] extends string
+    ? T[K]
+    : T[K] extends object
+      ? StringUnion<T[K]>
+      : never
+}[keyof T]
