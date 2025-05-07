7cb6ecb5a18d1a21eb422c94cbe14ec990d115ed
radekskrabucha <rskrabucha13@gmail.com>
2025-05-07 18:20:05 +0200

    feat(api/error): enhance error handling with ApiError class and createErrorResponse utility

--- apps/api/src/utils/error.ts
+++ apps/api/src/utils/error.ts
@@ -1,6 +1,25 @@
-type ErrorResponse<T extends object> = {
-  message: string
-} & T
+import {
+  ApiError as ApiErrorGeneric,
+  createErrorResponseFactory,
+  type ErrorData as ErrorDataGeneric
+} from '@monorepo-starter/utils/api-error'
+import type { StringUnion } from '@monorepo-starter/utils/types'
 
-export const errorResponse = <T extends object>(response: ErrorResponse<T>) =>
-  response
+export const ApiErrorType = {
+  example: {
+    invalidQuery: 'invalid_query'
+  }
+} as const
+
+export type ApiErrorType = StringUnion<typeof ApiErrorType>
+export type ExampleApiErrorType = StringUnion<typeof ApiErrorType.example>
+
+export type ErrorData<T extends ApiErrorType> = ErrorDataGeneric<T>
+
+export class ApiError<T extends ApiErrorType> extends ApiErrorGeneric<T> {
+  constructor(message: string, type: T) {
+    super(message, type)
+  }
+}
+
+export const createErrorResponse = createErrorResponseFactory<ApiErrorType>()
