4a124a0b2f6a634aece60645d765c334c05b380a
radekskrabucha <rskrabucha13@gmail.com>
2025-05-07 17:30:26 +0200

    feat(utils): add ApiError class and utility functions for error handling

---
+++ packages/utils/src/api-error.ts
@@ -0,0 +1,37 @@
+export class ApiError<T extends string = string> extends Error {
+  public readonly type: T
+
+  constructor(message: string, type: T) {
+    super(message)
+    this.name = 'ApiError'
+    this.type = type
+  }
+}
+
+export type ErrorBaseData<T extends string> = {
+  message: string
+  type: T
+}
+export type ErrorExtraData = Record<string, unknown>
+
+export const createErrorResponse = <
+  T extends string,
+  Extra extends ErrorExtraData = {}
+>(
+  message: string,
+  type: T,
+  extra?: Extra
+) =>
+  ({
+    message,
+    type,
+    ...(extra || {})
+  }) as ErrorBaseData<T> & Extra
+
+export const createErrorResponseFactory = <T extends string>() => {
+  return <Extra extends ErrorExtraData = {}>(
+    message: string,
+    type: T,
+    extra?: Extra
+  ) => createErrorResponse<T, Extra>(message, type, extra)
+}
