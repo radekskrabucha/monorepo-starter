d9fe2b307d3a7d81d958d8de3a003d7ed70d7a40
radekskrabucha <rskrabucha13@gmail.com>
2025-05-07 19:52:06 +0200

    fix(Greeting): replace inline error message with DefaultErrorFallback component for improved error handling

--- apps/web/app/features/home/components/Greeting.tsx
+++ apps/web/app/features/home/components/Greeting.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
+import { DefaultErrorFallback } from '~web/components/DefaultErrorFallback'
 import { QueryBoundary } from '~web/components/QueryBoundary'
 import { getGreetingQueryOptions } from '../actions'
 
@@ -9,7 +10,7 @@ export const Greeting = () => {
     <QueryBoundary
       query={getGreetingQuery}
       loadingFallback={<p>Loading...</p>}
-      errorFallback={({ error }) => <p>Error: {error.message}</p>}
+      errorFallback={({ error }) => <DefaultErrorFallback error={error} />}
     >
       {data => <h3 className="text-lg">{data.message}</h3>}
     </QueryBoundary>
