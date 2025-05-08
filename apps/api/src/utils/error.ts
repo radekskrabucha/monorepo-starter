a20d75ee0f9525d311be5bd8b9daa9ff6e56803f
radekskrabucha <rskrabucha13@gmail.com>
2025-05-08 13:47:03 +0200

    feat(api/error): add isApiError type guard and GenericApiErrorType for improved error handling

--- apps/api/src/utils/error.ts
+++ apps/api/src/utils/error.ts
@@ -5,6 +5,7 @@ import {
 } from '@monorepo-starter/utils/api-error'
 import type { StringUnion } from '@monorepo-starter/utils/types'
 
+// Here define all API error types/codes so you can handle it accordingly on client
 export const ApiErrorType = {
   generic: {
     unknown: 'unknown',
@@ -16,6 +17,7 @@ export const ApiErrorType = {
 } as const
 
 export type ApiErrorType = StringUnion<typeof ApiErrorType>
+export type GenericApiErrorType = StringUnion<typeof ApiErrorType.generic>
 export type ExampleApiErrorType = StringUnion<typeof ApiErrorType.example>
 
 export type ErrorData<T extends ApiErrorType = ApiErrorType> =
@@ -25,6 +27,12 @@ export class ApiError<T extends ApiErrorType> extends ApiErrorGeneric<T> {
   constructor(message: string, type: T) {
     super(message, type)
   }
+}
+
+export const isApiError = <T extends ApiErrorType = ApiErrorType>(
+  error: unknown
+): error is ApiError<T> => {
+  return error instanceof ApiError
 }
 
 export const createErrorResponse = createErrorResponseFactory<ApiErrorType>()
