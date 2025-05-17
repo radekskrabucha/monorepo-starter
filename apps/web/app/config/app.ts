efdd1889726abe7b478965032e305f32b50c3858
radekskrabucha <rskrabucha13@gmail.com>
2025-05-17 23:48:45 +0200

    feat: update app configuration to use dynamic app name and add SEO metadata generation

--- apps/web/app/config/app.ts
+++ apps/web/app/config/app.ts
@@ -1,6 +1,15 @@
+import { appName as _appName } from '@monorepo-starter/utils/app-config'
+import { seoFactory } from '@monorepo-starter/utils/seo'
 import type { StringWithAutoCompleteOptions } from '@monorepo-starter/utils/types'
 
-export const appName = 'Monorepo Starter'
+export const appName = _appName
+export const description =
+  'A comprehensive monorepo starter template with integrated tools and configurations for efficient development.'
+
+export const seo = seoFactory({
+  titleSuffix: appName,
+  description
+})
 
 export const ExternalLinks = {
   X: 'https://x.com/radek_1313',
