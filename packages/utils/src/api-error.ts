b6d2aea23e526d863d3e9f888426728394c65710
radekskrabucha <rskrabucha13@gmail.com>
2025-05-17 23:39:26 +0200

    refactor: enhance ApiError class and related error handling utilities with statusCode and improved type definitions

--- packages/utils/src/api-error.ts
+++ packages/utils/src/api-error.ts
@@ -1,37 +1,94 @@
+import type { Context } from 'hono'
+import type {
+  ClientErrorStatusCode,
+  ServerErrorStatusCode
+} from 'hono/utils/http-status'
+import { FetchError } from 'ofetch'
+
+export type ErrorStatusCode = ServerErrorStatusCode | ClientErrorStatusCode
+
 export class ApiError<T extends string = string> extends Error {
   public readonly type: T
+  public readonly statusCode: ErrorStatusCode
 
-  constructor(message: string, type: T) {
+  constructor(message: string, type: T, statusCode: ErrorStatusCode) {
     super(message)
     this.name = 'ApiError'
     this.type = type
+    this.statusCode = statusCode
   }
+}
+export const isApiError = <T extends string>(
+  error: unknown
+): error is FetchError<ErrorData<T, ErrorExtraData>> => {
+  return (
+    error instanceof FetchError &&
+    'type' in error.data &&
+    'message' in error.data &&
+    'statusCode' in error.data
+  )
 }
 
 export type ErrorExtraData = Record<string, unknown>
-export type ErrorData<T extends string> = {
+export type ErrorData<T extends string, Extra extends ErrorExtraData> = {
   message: string
   type: T
-} & ErrorExtraData
+  statusCode: ErrorStatusCode
+} & Extra
 
+type CreateErrorResponseArgs<
+  T extends string,
+  Extra extends ErrorExtraData = {}
+> = {
+  type: T
+  statusCode: ErrorStatusCode
+  message: string
+  extra?: Extra
+}
 export const createErrorResponse = <
   T extends string,
   Extra extends ErrorExtraData = {}
->(
-  message: string,
-  type: T,
-  extra?: Extra
-) =>
-  ({
-    message,
-    type,
-    ...(extra || {})
-  }) as ErrorData<T>
-
+>({
+  message,
+  statusCode,
+  type,
+  extra
+}: CreateErrorResponseArgs<T, Extra>): ErrorData<T, Extra> => ({
+  type,
+  statusCode,
+  message,
+  ...(extra || ({} as Extra))
+})
 export const createErrorResponseFactory = <T extends string>() => {
   return <Extra extends ErrorExtraData = {}>(
-    message: string,
-    type: T,
-    extra?: Extra
-  ) => createErrorResponse<T, Extra>(message, type, extra)
+    args: CreateErrorResponseArgs<T, Extra>
+  ): ErrorData<T, Extra> => createErrorResponse<T, Extra>(args)
+}
+
+type ErrorResponseArgs<T extends string, Extra extends ErrorExtraData = {}> = {
+  c: Context
+} & CreateErrorResponseArgs<T, Extra>
+export const errorResponse = <
+  T extends string,
+  Extra extends ErrorExtraData = {}
+>({
+  c,
+  message,
+  statusCode,
+  type,
+  extra
+}: ErrorResponseArgs<T, Extra>) =>
+  c.json(
+    {
+      type,
+      statusCode,
+      message,
+      ...((extra || {}) as Extra)
+    } satisfies ErrorData<T, Extra>,
+    statusCode
+  )
+export const errorResponseFactory = <T extends string>() => {
+  return <Extra extends ErrorExtraData = {}>(
+    args: ErrorResponseArgs<T, Extra>
+  ) => errorResponse<T, Extra>(args)
 }
