d04a2d6e84e6a7df4e27579b250a40a8c6d4fba3
radekskrabucha <rskrabucha13@gmail.com>
2025-05-17 23:42:36 +0200

    feat: add time utility functions and constants for time conversions

---
+++ packages/utils/src/time.ts
@@ -0,0 +1,9 @@
+export const ONE_MILLISECOND = 0.001
+export const ONE_SECOND = 1
+export const ONE_MINUTE = ONE_SECOND * 60
+export const ONE_HOUR = ONE_MINUTE * 60
+export const ONE_DAY = ONE_HOUR * 24
+export const ONE_WEEK = ONE_DAY * 7
+
+export const secondsToMs = (seconds: number) => seconds * 1000
+export const msToSeconds = (ms: number) => ms / 1000
