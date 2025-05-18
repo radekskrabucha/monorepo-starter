ee7862a0e037c64ef171db95f9e8aab85747a285
radekskrabucha <rskrabucha13@gmail.com>
2025-05-18 09:55:47 +0200

    chore: add eslint-plugin-drizzle to package.json and update eslint configuration

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -1,9 +1,18 @@
 import { baseConfig } from '@monorepo-starter/eslint-config'
+import drizzle from 'eslint-plugin-drizzle'
 import globals from 'globals'
 
 export default [
   ...baseConfig,
   {
     languageOptions: { globals: globals.node }
+  },
+  {
+    plugins: {
+      drizzle
+    },
+    rules: {
+      ...drizzle.configs.recommended.rules
+    }
   }
 ]
