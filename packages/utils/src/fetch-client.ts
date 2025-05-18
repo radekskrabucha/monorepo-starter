d9704088a3f69e86b359ac5fb83238ab7cbfad54
radekskrabucha <rskrabucha13@gmail.com>
2025-05-18 11:41:23 +0200

    refactor(fetch-client): enhance inferJSON and inferText functions with improved type safety for response handling

--- packages/utils/src/fetch-client.ts
+++ packages/utils/src/fetch-client.ts
@@ -1,4 +1,6 @@
 import type { ClientResponse } from 'hono/client'
+import type { ResponseFormat } from 'hono/types'
+import type { StatusCode, SuccessStatusCode } from 'hono/utils/http-status'
 import { ofetch, type FetchOptions } from 'ofetch'
 import { ONE_MINUTE, secondsToMs } from '~utils/time.js'
 
@@ -11,11 +13,57 @@ export const fetchBasicClient = ofetch.create({
   ...FETCH_DEFAULT_CONFIG
 })
 
-export function inferJSON<T extends ClientResponse<unknown>>(data: T) {
-  type JsonData = Awaited<ReturnType<(typeof data)['json']>>
-  return data as unknown as JsonData
+type ExtractSuccessResponse<T> =
+  T extends ClientResponse<infer Data, infer Status, infer Format>
+    ? Status extends SuccessStatusCode
+      ? ClientResponse<Data, Status, Format>
+      : never
+    : never
+
+export async function inferJSON<
+  Data,
+  Status extends StatusCode,
+  Format extends ResponseFormat,
+  T extends ClientResponse<Data, Status, Format>,
+  SuccessResponse extends ExtractSuccessResponse<T> = ExtractSuccessResponse<T>
+>(
+  data: T
+): Promise<
+  SuccessResponse extends ClientResponse<infer Data, StatusCode, ResponseFormat>
+    ? Data
+    : never
+> {
+  const json = await data.json()
+
+  return json as SuccessResponse extends ClientResponse<
+    infer D,
+    StatusCode,
+    ResponseFormat
+  >
+    ? D
+    : never
 }
-export function inferText<T extends ClientResponse<unknown>>(data: T) {
-  type JsonData = Awaited<ReturnType<(typeof data)['text']>>
-  return data as unknown as JsonData
+
+export async function inferText<
+  Data,
+  Status extends StatusCode,
+  Format extends ResponseFormat,
+  T extends ClientResponse<Data, Status, Format>,
+  SuccessResponse extends ExtractSuccessResponse<T> = ExtractSuccessResponse<T>
+>(
+  data: T
+): Promise<
+  SuccessResponse extends ClientResponse<infer Data, StatusCode, ResponseFormat>
+    ? Data
+    : never
+> {
+  const text = await data.text()
+
+  return text as SuccessResponse extends ClientResponse<
+    infer D,
+    StatusCode,
+    ResponseFormat
+  >
+    ? D
+    : never
 }
